2092d72c00248ad4d2259f1d42bebf674225175d
Dmitry V <wel4692@gmail.com>
2020-04-22 17:31:14 +0300

    refactoring header js and html

--- src/saved_articles/index.js
+++ src/saved_articles/index.js
@@ -4,7 +4,7 @@ import { props } from '../js/constants/constants';
 
 const header = new Header('header_color_black', props);
 // временно
-props.isLoggedIn = true;
-props.userName = 'Дмитрий';
+header.propsUser.userName = 'Дмитрий';
+header.propsUser.isLoggedIn = true;
 //
 header.render();
